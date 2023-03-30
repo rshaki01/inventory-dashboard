cfbbe15d77c2793a7889cff11c018c455c3d2ab2
rshaki01 <rshaki01@gmail.com>
2023-03-30 00:50:01 -0400

    Added add item button

---
+++ components/AddForm.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AddForm = () => {
+  return (
+    <div className='my-2 p-2 grid grid-cols-3 items-center'>
+        <p>yo</p>
+        <p>yo</p>
+        <p>yo</p>
+
+
+    </div>
+  )
+}
+
+export default AddForm
--- pages/inventory.jsx
+++ pages/inventory.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { GlobalContext } from '@/context/GlobalState';
 import { useContext } from 'react';
-import { data } from 'autoprefixer';
+import AddForm from '../components/AddForm';
 
 
 const inventory = () => {
@@ -12,11 +12,13 @@ const inventory = () => {
     <div className='bg-gray-100 min-h-screen'>
         <div className='flex justify-between p-4'>
             <p className='text-2xl'>Inventory</p>
-            <h2>Welcome Back, Rafat</h2>
+            <button class="bg-transparent hover:bg-purple-500 text-purple-600 font-semibold hover:text-white py-2 px-4 border border-purple-600 hover:border-transparent rounded">
+              +
+            </button>        
         </div>
         <div className='px-4'>
           <p>Manage your inventory of shoes, clothes, and collectibles.</p>
-          <div className='w-full my-4 p-4 border rounded-lg bg-white h-[80vh] overflow-scroll'>
+          <div className='w-full my-4 p-4 border rounded-lg bg-white h-[65vh] overflow-scroll'>
             <div className='my-3 p-2 grid md:grid-cols-5 sm:grid-cols-3 grid-cols-2 items-center cursor-pointer'>
               <span>Name</span>
               <span>Purchase Price</span>
