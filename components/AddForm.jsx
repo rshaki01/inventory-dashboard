a1f2ea9cba7b72de471039353cdeb8dd997084ad
rshaki01 <rshaki01@gmail.com>
2023-04-01 00:21:11 -0400

    Updated modal UI

--- components/AddForm.jsx
+++ components/AddForm.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 
-const AddForm = ({onClose}) => {
+const AddForm = ({open, onClose}) => {
+  if (!open) return null
   return (
-    <div className='my-2 p-2 grid grid-cols-3 items-center'>
-        <form>
-            <input type="text" />
-        </form>
-
+    <div className="fixed inset-0 bg-black bg-opacity-50 transition-opacity duration-500 flex items-center justify-center">
+      <div className="w-1/2 bg-black bg-opacity-20 p-4">
+        <h1>Create new item</h1>
+        <div className='my-2 p-2 flex flex-col gap-2'>
+          <form>
+              <input className="rounded-lg border" type="text" />
+          </form>
+          <form>
+              <input type="text" />
+          </form>
+          <form>
+              <input type="text" />
+          </form>
+        </div>
+        <button onClick={onClose}>x</button>
+      </div>
     </div>
   )
 }
--- pages/inventory.jsx
+++ pages/inventory.jsx
@@ -9,25 +9,18 @@ const inventory = () => {
   const { inventory } = useContext(GlobalContext);
   const [showModal, setShowModal] = useState(false);
 
-  const handleOpenModal = () => {
-    if (showModal) {
-      setShowModal(false);
-    } else {
-      setShowModal(true);
-    }
-  }
+
 
   return (
     <div className='bg-gray-100 min-h-screen'>
         <div className='flex justify-between p-4'>
             <p className='text-2xl'>Inventory</p>
-            <button onClick={handleOpenModal} class="bg-transparent hover:bg-purple-500 text-purple-600 font-semibold hover:text-white py-2 px-4 border border-purple-600 hover:border-transparent rounded">
+            <button onClick={() => setShowModal(true)}class="bg-transparent hover:bg-purple-500 text-purple-600 font-semibold hover:text-white py-2 px-4 border border-purple-600 hover:border-transparent rounded">
               +
             </button>        
         </div>
         <div className='px-4'>
           <p>Manage your inventory of shoes, clothes, and collectibles.</p>
-          {showModal && <AddForm/> }
           <div className='w-full my-4 p-4 border rounded-lg bg-white h-[65vh] overflow-scroll'>
             <div className='my-3 p-2 grid md:grid-cols-5 sm:grid-cols-3 grid-cols-2 items-center cursor-pointer'>
               <span>Name</span>
@@ -49,6 +42,7 @@ const inventory = () => {
             </ul>
           </div>
         </div>
+        <AddForm open={showModal} onClose={() => setShowModal(false)}/>
     </div>
 
   )
